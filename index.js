c471a74b0f71155c27bbf482c73e2555eee0dcfc
EYAL HARTMAN <EYALHAR@rf.local>
2019-08-19 12:02:02 +0300

    integration

--- index.js
+++ index.js
@@ -7,12 +7,12 @@ const request = require('request');
 // ==============================
 
 const autoStart = false;
-const antsServer = '10.51.128.52'; // localhost
+const antsServer = 'localhost'; //'10.51.128.52'; // localhost
 const uiServer = 'localhost'; // localhost
 
 var TelemetryMessage;
 var totalAnts = 2;
-var totalMoves = 6;
+var totalMoves = 7;
 
 let counter1 = -1;
 let payloads1 = [];
@@ -55,9 +55,18 @@ function startupServer(){
             reportCompletedToUi(1); // position the first ant on the grid
             sendCommandsToRealAnts(); // auto pass the data to the real ants server
         }
-        if((message + '').startsWith('ANTS:FINISH_')){
-            reportCompletedToUi(message.replace('ANTS:FINISH_', ''));
+        if((message + '')=== 'ANTS:FINISH_1'){
+            reportCompletedToUi(1);
         }
+        if((message + '')=== 'ANTS:FINISH_2'){
+            reportCompletedToUi(2);
+        }
+        if((message + '')=== 'ANTS:FINISH_3'){
+            reportCompletedToUi(3);
+        }
+        //if((message + '').startsWith('ANTS:FINISH_')){
+        //    reportCompletedToUi((message + '').replace('ANTS:FINISH_', ''));
+        //}
     });
  
     client.bind(1337);
@@ -69,7 +78,7 @@ function sendCommandsToRealAnts(){
             if(totalAnts >= 1) sendToRealAntServer(1, commands1[i]);
             if(totalAnts >= 2) sendToRealAntServer(2, commands2[i]);
             if(totalAnts >= 3) sendToRealAntServer(3, commands3[i]);
-        }, i * 100);
+        }, i * 1000);
     }
 };
 
@@ -147,21 +156,21 @@ function sendTelemetry(payload){
 
 function initPayloads(){
     // 1 (starts on map and moves to first T junction, then becomes trans)
-    payloads1.push({id: 1, x: 2, y: 30, angle: 0, ll:'wall', ul:'open', rl:'open', bl:'entry', battery:100, type:'scout'});  //1
-    payloads1.push({id: 1, x: 2, y: 29, angle: 0, ll:'wall', ul:'open', rl:'open', bl:'open', battery:99, type:'scout'});  //2
-    payloads1.push({id: 1, x: 2, y: 28, angle: 0, ll:'wall', ul:'open', rl:'open', bl:'open', battery:98, type:'scout'});  //3
-    payloads1.push({id: 1, x: 2, y: 27, angle: 0, ll:'wall', ul:'open', rl:'open', bl:'open', battery:97, type:'scout'});  //4
-    payloads1.push({id: 1, x: 2, y: 27, angle: 90, ll:'wall', ul:'open', rl:'open', bl:'open', battery:96, type:'scout'});  //5
-    payloads1.push({id: 1, x: 2, y: 27, angle: 90, ll:'wall', ul:'open', rl:'open', bl:'open', battery:95, type:'trans'}); //6
-    payloads1.push({id: -1, x: 2, y: 27, angle: 90, ll:'wall', ul:'open', rl:'open', bl:'open', battery:94, type:'trans'}); //7
+    payloads1.push({id: 1, x: 2, y: 30, angle: 0, ll:'wall', ul:'open', rl:'open', bl:'entry', battery:100, type:'scout'});  //0
+    payloads1.push({id: 1, x: 2, y: 29, angle: 0, ll:'wall', ul:'open', rl:'open', bl:'open', battery:99, type:'scout'});  //1
+    payloads1.push({id: 1, x: 2, y: 28, angle: 0, ll:'wall', ul:'open', rl:'open', bl:'open', battery:98, type:'scout'});  //2
+    payloads1.push({id: 1, x: 2, y: 27, angle: 0, ll:'wall', ul:'open', rl:'open', bl:'open', battery:97, type:'scout'});  //3
+    payloads1.push({id: 1, x: 2, y: 27, angle: 90, ll:'wall', ul:'open', rl:'open', bl:'open', battery:96, type:'trans'});  //4
+    payloads1.push({id: 1, x: 2, y: 27, angle: 90, ll:'wall', ul:'open', rl:'open', bl:'open', battery:95, type:'trans'}); //5
+    payloads1.push({id: -1, x: 2, y: 27, angle: 90, ll:'wall', ul:'open', rl:'open', bl:'open', battery:94, type:'trans'}); //6
 
-    commands1.push('f1');
-    commands1.push('f1');
-    commands1.push('f1');
-    commands1.push('r1');
-    commands1.push('NULL');
-    commands1.push('NULL');
-    commands1.push('NULL');
+    commands1.push('b1');  // 1
+    commands1.push('b1');  // 2
+    commands1.push('b1');  // 3
+    commands1.push('r1');  // 4
+    commands1.push('NULL'); // 5
+    commands1.push('NULL'); // 6
+    commands1.push('NULL'); // 7
 
     // 2 (starts outside, moves past ant 1)
     payloads2.push({id: 2, x: 3, y: 30, angle: 0, ll:'open', ul:'open', rl:'open', bl:'entry', battery:100, type:'scout'});  //1
@@ -172,13 +181,13 @@ function initPayloads(){
     payloads2.push({id: 2, x: 3, y: 26, angle: 90, ll:'open', ul:'wall', rl:'open', bl:'open', battery:95, type:'scout'}); //6
     payloads2.push({id: 2, x: 4, y: 26, angle: 90, ll:'open', ul:'wall', rl:'open', bl:'open', battery:94, type:'scout'}); //7
 
-    commands2.push('f1');
-    commands2.push('f1');
-    commands2.push('f1');
-    commands2.push('f1');
-    commands2.push('f1');
+    commands2.push('b1');
+    commands2.push('b1');
+    commands2.push('b1');
+    commands2.push('b1');
+    commands2.push('b1');
     commands2.push('r1');
-    commands2.push('f1');
+    commands2.push('b1');
     
     // total moves
     totalMoves = payloads1.length;
